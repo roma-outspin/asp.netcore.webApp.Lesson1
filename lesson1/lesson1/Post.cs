c49add8a529a1aeffbcb997f0433ed30cb1fe781
roma_outspin <roma_outspin@mail.ru>
2022-02-05 18:34:41 +0300

    hw created

---
+++ lesson1/lesson1/Post.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Threading.Tasks;
+
+namespace lesson1
+{
+    internal class Post
+    {
+
+        public int userId { get; set; }
+
+        public int id { get; set; }
+
+        public string title { get; set; }
+
+        public string body { get; set; }
+
+        public override string ToString()
+        {
+            return $"{userId}\r\n" +
+                $"{id}\r\n" +
+                $"{title}\r\n" +
+                $"{body}\r\n";
+        }
+    }
+}
---
+++ lesson1/lesson1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace lesson1
+{
+    class Program
+    {
+        static readonly HttpClient _jsonPHClient = new HttpClient();
+        static readonly string filePath = "posts.txt";
+
+        static async Task Main(string[] args)
+        {
+            for (int i = 4; i < 14; i++)
+            {
+                await GetResponse(i);
+            }
+           
+
+        }
+
+        static async Task GetResponse(int postNumber)
+        {
+            var response = await _jsonPHClient.GetAsync(@"https://jsonplaceholder.typicode.com/posts/"+postNumber);  
+            var content = await response.Content.ReadAsStringAsync();
+            var result = JsonSerializer.Deserialize<Post>(content);
+            await File.AppendAllTextAsync(filePath, result + "\r\n");
+        }
+
+
+    }
+}
